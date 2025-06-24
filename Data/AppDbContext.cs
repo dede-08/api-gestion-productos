201221812509237d3a65b5bcfe01b2d3a1e30d7f
dede-08 <benandrealanyaguevara@gmail.com>
2025-06-24 13:01:57 -0500

    carga del proyecto

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api_gestion_productos.Models;
+
+namespace api_gestion_productos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<Product> Products { get; set; }
+    }
+}
